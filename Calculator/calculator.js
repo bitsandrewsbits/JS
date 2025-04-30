e1f64c252a002f5bd45a09cee8a53bb0fb398079
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-04-30 21:53:49 +0300

    continue changing calculator functions.

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,43 +1,23 @@
 let selectorOperations = document.querySelector(".area_Operations");
 let objResults = document.querySelector(".area_Results");
 
+let math_operators = ['+', '-', '/', '*']
 let arithm_expression_elems = [];
-let arrOperators = [];
+let input_operators = [];
 let arithm_operands_group_by_operators = {};
 let numberN = 0; //start number;
 let tmpNum = []; //array for digits in number;
 
 let digit_regex = /\d/;
-let special_options = {}
-special_options["Del All"] = DeleteAllWindowSymbols
-special_options["Del One"] = DeleteOneWindowSymbol
+let special_options = {};
 
 selectorOperations.addEventListener('click', function(e){
   let elem = e.target.textContent; //get digit or operators(spec buttons);
 
-  function calculating(arrNum, arrOpers){
+  function calculating(input_elems, operators_order){
     // TODO: change entire func
-
-    let lenArrOpers = arrOpers.length;
-    let result = arrNum[0];
-    let tmpRes = 0;
-    let i = 0;
-
-    for (let i = 0; i < lenArrOpers; i++){
-      switch(arrOpers[i]){
-        case '+': result += arrNum[i + 1];
-          break;
-        case '-': result -= arrNum[i + 1];
-          break;
-        case '*': result *= arrNum[i + 1];
-          break;
-        case '/': result /= arrNum[i + 1];
-          break;
-        default: 'Unknown operation!';
-      }
-    }
-
-    return result;
+    GroupOperandsByOperators()
+   
   }
 
   function createNumber(arrDigits){
@@ -58,71 +38,106 @@ selectorOperations.addEventListener('click', function(e){
   }
 
   function DeleteOneWindowSymbol(){
-    console.log(objResults.innerHTML);
     if (objResults.innerHTML.length > 0){
-      objResults.innerHTML = objResults.innerHTML.slice(0, -2);
+      objResults.innerHTML = objResults.innerHTML.slice(0, -1);
     }
   }
+
+  special_options["Del All"] = DeleteAllWindowSymbols;
+  special_options["Del One"] = DeleteOneWindowSymbol;
 
   function AddInputNumberToArithmElementsArr(){
     objResults.innerHTML = objResults.innerHTML + ' ' + elem + ' ';
     numberN = createNumber(tmpNum);
     // console.log(numberN);
     arithm_expression_elems.push(numberN);
-    console.log(arrNumbers);
-    console.log(arrOperators);
     tmpNum = [];
   }
 
   function AddOperatorToElemOperatorArrs(){
     arithm_expression_elems.push(elem);
-    arrOperators.push(elem);
+    input_operators.push(elem);
   }
 
-  function DefineOrderOfCalculation(){
-    let arrOperators_copy = arrOperators.copy();
+  function GetOperatorsOrderOfCalculation(){
+    let input_operators_copy = input_operators;
     operators_calculation_order = [];
-    for (let i = 0; i < arrOperators_copy.length; i++){
-      if (arrOperators[i] == "*" || arrOperators[i] == "/"){
+    for (let i = 0; i < input_operators_copy.length; i++){
+      if (input_operators[i] == "*" || input_operators[i] == "/"){
         operators_calculation_order.push(
-          arrOperators.pop(i)
+          input_operators[i]
         );
+        input_operators.splice(i, input_operators[i]);
       }
     }
-    arrOperators = operators_calculation_order;
+    console.log('Remain input operators:' + input_operators)
+    operators_calculation_order.concat(input_operators);
+    console.log('Operators order:' + operators_calculation_order)
+    return operators_calculation_order;
   }
 
   function GroupOperandsByOperators(){
-    for (let i = 0; i < arrOperators.length; i++){
-
+    for (let i = 0; i < input_operators.length - 1; i++){
+      operator_index_in_expres_elems = arithm_expression_elems.indexOf(
+        input_operators[i]
+      )
+      left_operand = arithm_expression_elems[operator_index_in_expres_elems - 1]
+      right_operand = arithm_expression_elems[operator_index_in_expres_elems + 1]
+      arithm_expression_elems.splice(operator_index_in_expres_elems - 1, left_operand)
+      arithm_expression_elems.splice(operator_index_in_expres_elems + 1, right_operand)
+      
+      arithm_operands_group_by_operators[input_operators[i]] = [
+        left_operand, right_operand
+      ]
     }
+    console.log('Group By Operators:')
+    console.log(arithm_operands_group_by_operators)
   }
 
   function ShowResultInWindow(){
+    operators_calc_order = GetOperatorsOrderOfCalculation()
     objResults.innerHTML = objResults.innerHTML + elem;
     numberN = createNumber(tmpNum);
-    arrNumbers.push(numberN);
-    console.log(calculating(arrNumbers, arrOperators));
-    objResults.innerHTML = calculating(arrNumbers, arrOperators);
+    arithm_expression_elems.push(numberN);
+    objResults.innerHTML = calculating(arithm_expression_elems, operators_calc_order);
   }
 
-  function CheckPreviousSymbolNotOperator(){
-    if ()
+  function CheckSymbolIsOperator(symbol){
+    if (math_operators.includes(symbol)){
+      return true;
+    }
+    return false;
+  }
+
+  function CheckPreviousSymbolIsNotOperator(){
+    if (arithm_expression_elems.length == 0){
+      return true;
+    }
+    else {
+      console.log(arithm_expression_elems)
+      console.log('Last input element: ' + arithm_expression_elems.slice(-1))
+      if (CheckSymbolIsOperator(arithm_expression_elems.slice(-1))){
+        return false;
+      }
+      return true;
+    }
   }
 
   if (elem == "Del All" || elem == "Del One"){
     special_options[elem]();
   }
   if (digit_regex.test(elem)){
+    // console.log('Only digit:' + elem)
     tmpNum.push(parseInt(elem));
     objResults.innerHTML = objResults.innerHTML + elem;
   }
   if (elem == '='){
     ShowResultInWindow();
   }
-  else {
-    if CheckPreviousSymbolNotOperator(elem){
-      AddInputNumberToArithmElementsArr();
+  if (CheckSymbolIsOperator(elem)){
+    AddInputNumberToArithmElementsArr();
+    if (CheckPreviousSymbolIsNotOperator()){
+      AddOperatorToElemOperatorArrs();
     }
   }
 });
